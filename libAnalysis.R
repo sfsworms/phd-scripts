91509ae049733efbba055b33c66c161a3c0ecaaa
Sebastian Worms <sebastianworms@gmail.com>
2022-05-19 17:26:57 +0200

    Added a line of code to load the count file

--- libAnalysis.R
+++ libAnalysis.R
@@ -6,3 +6,7 @@ source("function.R") #Functions used in the script
 
 ##This is still me playing with the count paper.
 
+setwd("C:/Users/worms/Dropbox/PhD/Scripts/CylicPeptidePipeline")
+
+count <- read.csv(choose.files())
+ #From the count file of the count paper I can go and do the rest of hte graphes. 
