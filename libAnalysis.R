154c8c5614cb1094e0f65827ca9fb581f1c3bd4f
Sebastian Worms <sebastianworms@gmail.com>
2022-05-19 16:52:12 +0200

    Up the library file

--- libAnalysis.R
+++ libAnalysis.R
@@ -1,31 +1,8 @@
-#So, let me play with fake libs.
+## Analysis project
 
-#Load libraries
-library(tidyverse)
-library(Biostrings)
+setwd(dir="C:/Users/worms/Dropbox/PhD/Scripts/CylicPeptidePipeline")
+source("library.R") #Install and/or load needed libraries
+source("function.R") #Functions used in the script
 
-#Create functions I want to create a random peptide
-createPeptide <- function(size = 8){
-  aaList <- AA_ALPHABET[c(1:20,27)]
-  peptide <- aaList[sample(1:length(aaList), size)] %>% 
-    unlist %>%
-    paste(.,collapse="")
-  return(peptide)
-}
-
-createLibrary <- function(n = 1, size = 8){
-  library <- vector()
-  for(i in 1:n){
-  library <- c(library,createPeptide(size))
-  } 
-  library <- AAStringSet(x = library)
-  return(library)
-}
-
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-  
-BiocManager::install("universalmotif")
-
+##This is still me playing with the count paper.
 
