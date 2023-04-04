6ec23b2f71902306b6474dfbbf4d502fc0adb6bb
Sebastian Worms <sebastian.worms@uclouvain.be>
2023-04-04 12:56:30 +0200

    Added a section to print out the graph of quality

--- CyclicPeptidePipeline/scripts/reads_merging_dada2.R
+++ CyclicPeptidePipeline/scripts/reads_merging_dada2.R
@@ -5,7 +5,7 @@
 library(tidyverse)
 library(dada2)
 
-path <- "C:/Users/worms/NGS Data/2022.06.07_drift_seq/90-666155004b/00_fastq/NNK"
+path <- "C:\Users\worms\NGS Data\2022.06.07_drift_seq\90-666155004b\00_fastq\NNB"
 list.files(path)
 
 # Forward and reverse fastq filenames have format: SAMPLENAME_R1_001.fastq and SAMPLENAME_R2_001.fastq
@@ -26,7 +26,12 @@ sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
 # typically all the same length, hence the flat red line). Here the quality is mostly good. Also
 # quality is chunky here, with values at 13, 25 and 37.
 
-# plotQualityProfile(c(fnFs,fnRs))
+
+png(file = file.path(path,"quality_plot.png"), bg = "transparent",  width = 600)
+
+plotQualityProfile(c(fnFs,fnRs))
+
+dev.off()
 
 #Quality is pretty good here. I commented out the file because it takes time.
 
@@ -37,6 +42,7 @@ sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
 filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
 filtRs <- file.path(path, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
 names(filtFs) <- sample.names
+
 names(filtRs) <- sample.names
 
 #The below truncate the fw and reverse reads. If assigned, "out" is a list of the files and reads obtained.
@@ -44,25 +50,24 @@ names(filtRs) <- sample.names
 out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=c(145,145), #Those are the cutoff for fw and rv
                      maxN=0, maxEE=c(3,5), truncQ=c(10,10), rm.phix=TRUE,
                      compress=TRUE, multithread=FALSE) # On Windows set multithread=FALSE
-head(out)
 
 write.csv(x = out, file = file.path(path,"filter_report.csv"))
 
 # The DADA2 algorithm makes use of a parametric error model (err) and every amplicon dataset has a different set of error rates. The learnErrors method learns this error model from the data
-
-errF <- learnErrors(filtFs, multithread=TRUE)
-
-errR <- learnErrors(filtRs, multithread=TRUE)
-
-# We are now ready to apply the core sample inference algorithm to the filtered and trimmed sequence data.
-
-dadaFs <- dada(filtFs, err=errF, multithread=TRUE)
-dadaRs <- dada(filtRs, err=errR, multithread=TRUE)
-
-# We now merge the forward and reverse reads together to obtain the full denoised sequences. Merging is performed by aligning the denoised forward reads with the reverse-complement of the corresponding denoised reverse reads, and then constructing the merged “contig” sequences. By default, merged sequences are only output if the forward and reverse reads overlap by at least 12 bases, and are identical to each other in the overlap region (but these conditions can be changed via function arguments).
-
-mergers <- mergePairs(dadaFs, filtFs, dadaRs, filtRs, verbose=TRUE)
-# Inspect the merger data.frame from the first sample
-
-head(mergers[[1]])
-
+# 
+# errF <- learnErrors(filtFs, multithread=TRUE)
+# 
+# errR <- learnErrors(filtRs, multithread=TRUE)
+# 
+# # We are now ready to apply the core sample inference algorithm to the filtered and trimmed sequence data.
+# 
+# dadaFs <- dada(filtFs, err=errF, multithread=TRUE)
+# dadaRs <- dada(filtRs, err=errR, multithread=TRUE)
+# 
+# # We now merge the forward and reverse reads together to obtain the full denoised sequences. Merging is performed by aligning the denoised forward reads with the reverse-complement of the corresponding denoised reverse reads, and then constructing the merged “contig” sequences. By default, merged sequences are only output if the forward and reverse reads overlap by at least 12 bases, and are identical to each other in the overlap region (but these conditions can be changed via function arguments).
+# 
+# mergers <- mergePairs(dadaFs, filtFs, dadaRs, filtRs, verbose=TRUE)
+# # Inspect the merger data.frame from the first sample
+# 
+# head(mergers[[1]])
+# 
