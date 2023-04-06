3f7fdc1bdd8b780f11453762e1969458ac7600dd
Sebastian Worms <sebastian.worms@uclouvain.be>
2023-04-06 14:36:06 +0200

    Added comments in the batch file running flash

--- cyclic_peptide_pipeline/other_tools/flash_test.bat
+++ cyclic_peptide_pipeline/other_tools/flash_test.bat
@@ -1,7 +1,9 @@
 @echo off
+rem This calls the FLASH software. INPUT_FOLDER should be a path to a folder with one fwr reads and the corresponding rv reads. The software assume forward reads ends in _F_filt.fastq.gz and the reverse reads in _R_filt.fastq.gz but have otherwise identical names. The naming scheme comes from dada2
+
 setlocal EnableDelayedExpansion
 
-set INPUT_FOLDER=C:\Users\worms\NGS Data\2022.06.07_drift_seq\90-666155004b\00_fastq\NNK\dada2_test\filtered
+set INPUT_FOLDER=C:\Users\worms\NGS Data\2022.06.07_drift_seq\90-666155004b\00_fastq\NNB\filtered\gen_5_glu
 set OUTPUT_FOLDER="%INPUT_FOLDER%\merged_reads"
 set INPUT_FOLDER="%INPUT_FOLDER%"
 
@@ -10,7 +12,7 @@ if not exist %OUTPUT_FOLDER% mkdir %OUTPUT_FOLDER%
 for %%f in (%INPUT_FOLDER%\*_F_filt.fastq.gz) do (
     set READ1=%%f
 	set "READ2=!READ1:_F_filt=_R_filt!"
-    flash -o merged -M 110  -d !OUTPUT_FOLDER! "!READ1!" "!READ2!"
+    flash -o merged -M 93  -d !OUTPUT_FOLDER! "!READ1!" "!READ2!" -z
 )
 
 
