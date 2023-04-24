cc192f0b117d819849f6c49c60b0d7da53d8fd9f
Sebastian Worms <sebastian.worms@uclouvain.be>
2023-04-24 10:34:10 +0200

    Changed to update to bioconductor 3.17

--- update_script.R
+++ update_script.R
@@ -2,13 +2,16 @@
 
 if (!requireNamespace("installr", quietly = TRUE))
   install.packages("installr")
+
 library(installr)
+
 updateR()
 
 update.packages()
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install(version = "3.16")
+
+BiocManager::install()
 
 Sys.setenv(LANG = "en")
