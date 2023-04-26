206774604027524ca2fef7bc33d25962c9744755
Sebastian Worms <sebastian.worms@uclouvain.be>
2023-04-26 17:24:53 +0200

    Edited the file to run in the background

--- cyclic_peptide_pipeline/scripts/peptide_extraction_NNK.R
+++ cyclic_peptide_pipeline/scripts/peptide_extraction_NNK.R
@@ -1,6 +1,6 @@
 ## This file serves to extract the peptide sequences from filtered and merged files.
 
-setwd(dir = "C:/Users/worms/Dropbox/phd_script/cyclic_peptide_pipeline")
+#setwd(dir = "C:/Users/worms/Dropbox/phd_script/cyclic_peptide_pipeline")
 source("functions/peptide_extraction_NNK_functions.R")  #Functions used in the script
 
 # Libraries need
@@ -8,14 +8,14 @@ library(ShortRead)
 library(tidyverse)
 
 # Directory store the directory with all the files.
-directory <- "C:/Users/worms/NGS Data/2022.06.07_drift_seq/90-666155004b/00_fastq/NNK/filtered_merged"
+directory <- "C:/Users/worms/ngs_data/2022_06_07_drift_seq/90-666155004b/00_fastq/NNB/filtered_merged"
 
 #Get a list of the merged .fastq files
 file_list <- list.files(directory) %>%  #Get files from directory
   grep(pattern = ".fastq.gz$", ., value = TRUE) #only the .Fastq
  
 
-destination <- file.path("C:/Users/worms/NGS Data/2022.06.07_drift_seq/90-666155004b/00_fastq/NNK") #Set where to put the fasta with peptides files.
+destination <- file.path("C:/Users/worms/ngs_data/2022_06_07_drift_seq/90-666155004b/00_fastq/NNB") #Set where to put the fasta with peptides files.
 
 tracking_df <- data.frame(n_reads = integer(),
                           n_short_peptide = integer(),
