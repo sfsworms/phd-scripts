8077b0a31979eaa475d224fe79202af4528762ca
Sebastian Worms <sebastian.worms@uclouvain.be>
2023-07-10 18:12:28 +0200

    I wrote a short script to merge experiments from multiple repetitions so they can be analysed together.

--- tecan_growth/merging_files.R
+++ tecan_growth/merging_files.R
@@ -2,4 +2,27 @@
  ## This function is used to merge datasets representing multiple repetitions of the same experiment.
 
 library(tidyverse) # tidyverse
-source(file = "C:/Users/worms/phd-scripts/tecan_growth/utils.R") # Load the functions for analysis of the tecan data
+
+script_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)# Get the path to this script.
+
+source(file = file.path(script_dir, "utils.R")) # Load the functions for analysis of the tecan data
+
+folders_names <- c("2023.06.28_test_select_ara", "2023.06.29_test_select_ara" )
+output_name <- "merged_ara.csv"
+
+directory <- "C:/Users/worms/Dropbox/data/tecan"
+
+tecan_data <- data.frame()
+
+for(i in seq_along(folders_names)){
+  folder_loc <- file.path(directory,folders_names[i])
+  new_data <- get_data_tecan(folder_loc = folder_loc) %>% # Load the tecan data
+    add_well_name(., folder_loc = folder_loc) %>%
+    add_experiment_details(., folder_loc = folder_loc) %>%
+    mutate(experiment = folders_names[i]) %>%
+    mutate(experiment_number = i)
+  
+  tecan_data <- rbind(tecan_data, new_data)
+}
+
+write.csv(tecan_data, file = file.path(directory, output_name))
