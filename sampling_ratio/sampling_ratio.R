6320a094f4363634534a3ea46492798635524979
Sebastian Worms - Laptop <sebastianworms@gmail.com>
2023-11-02 16:40:50 +0100

    Adding the file to compute sampling ratios

---
+++ sampling_ratio/sampling_ratio.R
@@ -0,0 +1,93 @@
+# Load required libraries
+library(ggplot2)
+library(tidyverse)
+
+# Function to simulate bacterial library
+simulate_library <- function(X, mean_count = 10) {
+  data.frame(clone_id = c(1:X),
+             clone_count = rpois(X, mean_count)*100) %>%
+  return()
+}
+
+# Function to subset bacterial library
+subset_library <- function(lib, Y, replacement = FALSE) {
+  if (sum(lib$clone_count) < Y) stop("Not enough bacteria to subset")
+  count_array <- lib %>% pull(clone_count)
+  
+  new_lib <- rep(0, nrow(lib))
+  
+  for (i in 1:Y) {
+    # Randomly select a clone based on its proportion in the library
+    selected_clone <- sample(1:length(count_array), size = 1, prob = count_array / sum(count_array))
+    new_lib[selected_clone] <- new_lib[selected_clone] + 1
+    if (!replacement) lib[selected_clone,2] <- lib[selected_clone,2] - 1
+  }
+  
+  return(data.frame(clone_id = c(1:X),
+                    clone_count = new_lib))
+}
+
+# Simulate a library of 100 different clones
+X <- 1e2
+original_library <- simulate_library(X)
+#original_library[1,2] <- 10000
+plot(original_library)
+
+# Subset 200 bacteria from the original library
+Y <- 10000
+subsetted_library <- subset_library(original_library, Y)
+plot(subsetted_library)
+
+
+# Plot the distributions
+df <- data.frame(rbind(original_library %>% 
+                         mutate(type = "original"),
+                        subsetted_library %>%
+                          mutate(type = "subsetted")
+                       )
+                 )
+
+ggplot(df, aes(x = clone_id, y = clone_count, fill = type)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Distribution of Bacterial Clones",
+       x = "Clone ID",
+       y = "Count")
+
+# Add normalized value
+df <- df %>% 
+  group_by(type) %>%
+  mutate(ratio = clone_count/sum(clone_count)) %>%
+  ungroup() %>% 
+  pivot_wider(names_from = type, values_from = c(ratio,clone_count))
+
+# Plot normalized values against each other
+ggplot(df, aes(x = ratio_original, y = ratio_subsetted)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +  # Optional: Add a linear fit
+  labs(title = "Comparison of Clone Ratio: Original vs Subsetted",
+       x = "Original Library Counts",
+       y = "Subsetted Library Counts")
+
+# Compute correlation coefficient
+
+cor(df$clone_count_original, df$clone_count_subsetted)
+
+# Do it at the function level
+subsetting_correlation <- function(sampling_ratio, X = 100){
+  ori_lib <- simulate_library(X) %>% pull()
+  sub_lib <- subset_library(ori_lib, X * sampling_ratio)
+  correlation <- cor(ori_lib$clone_count, sub_lib$clone_count)
+  return(correlation)
+  
+}
+
+ratio = c(1:10)
+
+cor_df <- data.frame(sampling_ratio = c(1:200)) %>%
+  mutate(correlation = lapply(sampling_ratio, subsetting_correlation))
+
+ggplot(cor_df, aes(x = sampling_ratio, y = correlation)) +
+  geom_point() +
+  labs(title = "Correlation of ratio after subsetting depending on sampling ratios",
+       x = "Sampling ratios",
+       y = "Correlation coefficient")
