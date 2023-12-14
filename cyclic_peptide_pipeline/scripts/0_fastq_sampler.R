6275e108c44ecb37dea6fab7a563718cf6b75c58
Sebastian Worms - Laptop <sebastianworms@gmail.com>
2023-12-14 22:30:47 +0100

    finished the script so that it can create an output folder, and to more easily choose the amount of sequences to samples

--- cyclic_peptide_pipeline/scripts/0_fastq_sampler.R
+++ cyclic_peptide_pipeline/scripts/0_fastq_sampler.R
@@ -11,14 +11,42 @@ if (!require("ShortRead")) {
   library(ShortRead)
 }
 
-## Link to the data
-input_directory <- file.path("") #This should link to the folder with the .fastq.gz
-output_directory <- file.path("C:/Users/worms/ngs_data/2022_06_07_drift_seq/90-666155004b/00_fastq/NNB")
+if (!require("fs")) {
+  install.packages("fs")
+  library(fs)
+}
+
+## Inputs
+### Folder to the data
+input_directory <- file.path("C:/Users/Sebastian Worms/ngs_data/NGS Ale/90-933598625/00_fastq") #This should link to the folder with the .fastq.gz
+output_directory <- file.path("C:/Users/Sebastian Worms/ngs_data/NGS Ale/90-933598625/00_fastq","sampled_fastq") #Directory where the samples will be stored
+
+### Number of sequences to sample
+n = 10e5
+
+# Check if the subfolder exists
+if (!dir_exists(output_directory )) {
+  # Create the subfolder if it does not exist
+  dir_create(output_directory )
+}
 
 ## Script
 
 ### Get the files
 
-#Get a list of the merged .fastq files
-file_list <- list.files(directory) %>%  #Get files from directory
+#Get a list of the .fastq files.
+file_list <- list.files(input_directory) %>%  #Get files from directory
   grep(pattern = ".fastq.gz$", ., value = TRUE) #only the .Fastq
+
+for(i in file_list){
+  stream <- FastqStreamer(file.path(input_directory,i), n = n)
+  on.exit(close(stream))
+  fq <- yield(stream)
+  writeFastq(fq, file.path(output_directory,
+                           paste0(gsub(".fastq.gz",
+                                       "_sample.fastq.gz",
+                                       i)
+                                  )
+                           )
+             )
+}
